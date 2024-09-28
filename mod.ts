96dd83e3c0a8ca0ee26e5ab525ca732c8ec235f3
masinc <masinc000@gmail.com>
2024-09-28 23:51:07 +0900

    Add deno.lock file and update mod.ts exports for range and array functions

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+export { range } from "./src/range.ts";
+export type { RangeError } from "./src/range.ts";
+
+export { arrayEquals } from "./src/arrayEquals.ts";
+export { arrayStartsWith } from "./src/arrayStartsWith.ts";
